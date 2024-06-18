f923d843d26988535f5a34eb99b1e6d8cbd0cc36
S. R. Mohammad Imran <raining101290@users.noreply.github.com>
2024-06-18 09:31:08 +0600

    update meta og

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css"
 import { AppContextProvider } from "./context"
 import PropTypes from "prop-types"
 import { Box } from "@mui/material"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -15,6 +16,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content="E-Shop online platform" />
+        <meta
+          property="og:description"
+          content="E-Shop is a largest shoppers platform"
+        />
+        <meta property="og:image" content="/logo.webp" />
+        <meta
+          property="og:url"
+          content="https://product-search-weld.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="E-Shop online platform" />
+        <meta
+          name="twitter:description"
+          content="E-Shop is a largest shoppers platform"
+        />
+        <meta name="twitter:image" content="/logo.webp" />
+      </Head>
       <body className={inter.className}>
         <Box>
           <Suspense>
